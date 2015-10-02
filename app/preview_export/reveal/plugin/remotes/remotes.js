e1816bbac176b0912635e5b684a8fb19ba2ccf2a
tobi <githubtobimuc@subalpa.de>
2015-10-02 21:08:12 +0200

    Added offline remotes

--- app/preview_export/reveal/plugin/remotes/remotes.js
+++ app/preview_export/reveal/plugin/remotes/remotes.js
@@ -34,6 +34,6 @@
             ;
         } );
 
-        head.js('https://raw.github.com/Remotes/Remotes/master/dist/remotes.ne.min.js');
+        head.js('./remotes.ne.min.js');
     }
 })(window);
